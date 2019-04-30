ece15e613b1e269d399bdc10174b0eaf7ba26556
jeffreyT <molterjt@miamioh.edu>
2019-04-30 11:29:29 -0400

    updated siteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jeff Molter`,
+    description: `Developer portfolio`,
+    author: `Jeff Molter`,
   },
   plugins: [
     'gatsby-plugin-emotion',
